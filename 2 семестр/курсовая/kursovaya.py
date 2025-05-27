83c4f6c6ae69294914be923a2704efc593c23826
OlegMamashev <rakpakjack@gmail.com>
2025-05-27 20:38:14 +0300

    .

--- 2 семестр/курсовая/kursovaya.py
+++ 2 семестр/курсовая/kursovaya.py
@@ -4,10 +4,11 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLineEdit,
 
 
 class Figure:
-    def __init__(self, position: tuple):
+    def __init__(self, position: tuple, board_size: int):
         self.position = position
+        self.board_size = board_size
 
-    def movements(self, board_size: int):
+    def movements(self):
         moves = [
             (-2, -1), (-2, 1),
             (-1, -2), (-1, 0), (-1, 2),
@@ -16,10 +17,10 @@ class Figure:
             (2, -1), (2, 1)
         ]
 
-        coords = []
+        coords = set()
         for i in moves:
-            if 0 <= self.position[0] + i[0] < board_size and 0 <= self.position[1] + i[1] < board_size:
-                coords.append((position[0] + i[0], position[1] + i[1]))
+            if 0 <= self.position[0] + i[0] < self.board_size and 0 <= self.position[1] + i[1] < self.board_size:
+                coords.add((self.position[0] + i[0], self.position[1] + i[1]))
         return coords
 
 
@@ -28,34 +29,84 @@ class Board:
         self.size = n
         self.figures = []
         self.block_coords = set()
+        self.placed_figures = []
+        self.placed_block_coords = set()
 
     def add_figure(self, figure: Figure) -> bool:
         if self.check_attack(figure.position) is None:
             return False
         self.figures.append(figure)
         self.block_coords.add(figure.position)
-        self.block_coords + figure.movements(self.size)
+        # self.block_coords + figure.movements()
+        self.block_coords.update(figure.movements())
+        return True
+
+    def add_placed_figure(self, figure: Figure) -> bool:
+        if self.check_attack(figure.position) is None:
+            return False
+        self.placed_figures.append(figure)
+        self.placed_block_coords.add(figure.position)
+        self.placed_block_coords.update(figure.movements())
         return True
 
     def check_attack(self, coord: tuple) -> tuple | None:
-        if coord not in self.block_coords:
+        # if coord not in self.block_coords and coord not in self.placed_block_coords:
+        if coord not in self.get_block_coords():
             return coord
         else:
             return None
 
-    def get_figure_pos(self):
-        return [figure.position for figure in self.figures]
+    def get_figure_pos(self) -> list:
+        return [figure.position for figure in self.figures] + [figure.position for figure in self.placed_figures]
+
+    def get_block_coords(self):
+        all_block_coords = self.block_coords.union(self.placed_block_coords)
+        return all_block_coords
+
+    def remove_figure(self) -> None:
+        # self.figures = []
+        # self.block_coords = set()
+        if len(self.figures) != 0:
+            del self.figures[-1]
+            self.block_coords = set()
+            for figure in self.figures:
+                self.block_coords.add(figure.position)
+                self.block_coords.update(figure.movements())
 
 
 class Main:
-    def __init__(self, place_fig_num):
+    def __init__(self, place_fig_num, board_size, placed_fig):
         self.place_fig_num = place_fig_num
+        self.board_size = board_size
+        self.placed_fig = placed_fig
+        self.board = Board(board_size)
         self.combination_number = 0
 
-    def place_fig(self):
+    def write_output(self):
         pass
 
+    def place_fig(self):
 
+        # Добавляем расставленные фигуры
+        if self.placed_fig:
+            for i in self.placed_fig:
+                self.board.add_placed_figure(Figure(i, self.board_size))
+
+        def counting(start_y, start_x):
+            if len(self.board.figures) == self.place_fig_num:
+                self.write_output()
+                print(self.board.get_figure_pos())
+
+                # Придумать как свзять рисовку первой комбинации
+                # if self.combination_number == 0:
+                #     pass
+
+            for y in range(start_y, self.board_size):
+                for x in range(start_x if y == start_y else 0, self.board_size):
+                    if self.board.add_figure(Figure((y, x), self.board_size)):
+                        counting(y, x)
+                        self.board.remove_figure()
+        counting(0, 0)
 
 
 class BoardGui:
@@ -168,6 +219,10 @@ class Gui(QMainWindow):
 
 
 if __name__ == '__main__':
+
+    test = Main(1, 3, [(2, 2)])
+    test.place_fig()
+
     app = QApplication(sys.argv)
     gui = Gui()
     gui.show()
